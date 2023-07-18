a03056730bbc5106652b30ee02060c2857f6ab6d
jcgorobat <132338842+jcgorobat@users.noreply.github.com>
2023-07-18 22:53:57 +0800

    Make pseudocode/comments for the actual game

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// 1. Make the computer chose it weapon (rock, paper, scissor)
+// 2. Ask the player his/her weapon of choice (rock paper scissor)
+// 3. Actual Game
+        //PLAYER PICKS ROCK
+        // a. If Player = Rock, CPU = Rock; TIE
+        // b. If Player = Rock, CPU = Paper; You Lose
+        // c. If Player = Rock, CPU = Scissor, You WiN
+        //PLAYER PICKS PAPER
+        // a. If Player = Paper, CPU = Rock; You WIN
+        // b. If Player = Paper, CPU = Paper; TIE
+        // c. If Player = Paper, CPU = Scissor; You LOSE
+        //PLAYER PICKKS SCISSOR
+        // a. If Player = Scissor, CPU = Rock; You Lose
+        // b. If Player = Scissor, CPU = Paper; You Win
+        // c. If Player = Scissor, CPU = Scissor; TIE
+//4. Play the game with Best-of-Five series. Show the Game #, Score for each round.
+//5. First player to score 3 wins the GAME.
