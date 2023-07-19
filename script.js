d308d62e7c5388b00bdb9da2742a94b4e4407162
jcgorobat <132338842+jcgorobat@users.noreply.github.com>
2023-07-19 22:07:10 +0800

    Make script for computer and playerSelection

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 // 1. Make the computer chose it weapon (rock, paper, scissor)
+
+function getComputerChoice () {
+        const weaponArray = ["ROCK", "PAPER", "SCISSORS"];
+                let randomIndex = Math.floor(Math.random() * weaponArray.length);
+                let randomWeapon = weaponArray[randomIndex];
+        return randomWeapon;
+}
+
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection)
+
 // 2. Ask the player his/her weapon of choice (rock paper scissor)
 
-let playerSelection = prompt("Please choose your weapon:\nROCK, PAPER, or SCISSOR?")
-
+let playerSelection = prompt("Please choose your weapon:\nROCK, PAPER, or SCISSORS?").toUpperCase();
+console.log(playerSelection);
 
 // 3. Actual Game
-        //PLAYER PICKS ROCK
-        // a. If Player = Rock, CPU = Rock; TIE
-        // b. If Player = Rock, CPU = Paper; You Lose
-        // c. If Player = Rock, CPU = Scissor, You WiN
+
+function playRound (playerSelection, computerSelection) {
+       //PLAYER PICKS ROCK
+        if (playerSelection == "ROCK") {
+                switch (computerSelection) {
+                        // a. If Player = Rock, CPU = Rock; TIE
+                        case ("ROCK"):
+                        return "TIE GAME! You both chose ROCK.";
+                        break;
+
+                        // b. If Player = Rock, CPU = Paper; You Lose
+                        case ("PAPER"):
+                        return "You LOSE! Computer chose PAPER. Paper beats Rock!";
+                        break;
+
+                         // c. If Player = Rock, CPU = Scissor, You WiN
+                        case ("SCISSORS"):
+                        return "You WIN! Computer chose SCISSORS. Rock beats Scissors!";
+                        break;
+                }                
+        } else {
+                console.log('Stop!');
+        }
+        }
+        
+        
+       
         //PLAYER PICKS PAPER
         // a. If Player = Paper, CPU = Rock; You WIN
         // b. If Player = Paper, CPU = Paper; TIE
@@ -16,6 +51,7 @@ let playerSelection = prompt("Please choose your weapon:\nROCK, PAPER, or SCISSO
         //PLAYER PICKKS SCISSOR
         // a. If Player = Scissor, CPU = Rock; You Lose
         // b. If Player = Scissor, CPU = Paper; You Win
-        // c. If Player = Scissor, CPU = Scissor; TIE
+        // c. If Player = Scissor, CPU = Scissor; TIE  
+        
 //4. Play the game with Best-of-Five series. Show the Game #, Score for each round.
 //5. First player to score 3 wins the GAME.
