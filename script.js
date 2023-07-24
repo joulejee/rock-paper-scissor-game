53410fcb0b1d06d6cc9862308a9796befbbd4c8b
jcgorobat <132338842+jcgorobat@users.noreply.github.com>
2023-07-24 23:00:22 +0800

    Create scoreboard for human and computer

--- script.js
+++ script.js
@@ -92,17 +92,16 @@ function game() {
         playRound(playerSelection, computerSelection)
         
         //b. Create a scoreboard for player and computer.
-        let 
+        let humanScore = 0;
+        let computerScore = 0;
+        let scoreBoard = `SCOREBOARD = YOU: ${humanScore} | COMPUTER: ${computerScore}`;
         
         //b. For every round, winner and loser will be displayed, and accumulated scores will be shown.
         // - if function returns WIN, +1 to the PLAYER.
-        if (playRound() == 'WIN')
                 // - if function returns TIE, do nothing.
                 // - if function returns LOSE, +1 to the COMPUTER.
         //c. Repeat playRound() five times.
-
 }
-
 //5. First player to score 3 wins the GAME.
 
               
